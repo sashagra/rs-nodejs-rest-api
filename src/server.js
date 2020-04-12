9a07f33deee1f6f5561e6bc621c1550a66c2da6b
Alex G <archon.san@gmail.com>
2020-04-12 13:36:13 +0300

    added log file path to info message

--- src/server.js
+++ src/server.js
@@ -1,14 +1,25 @@
 const { PORT } = require('./common/config');
 const app = require('./app');
 const { logger } = require('./app-services/logger');
+const path = require('path');
 
 process
   // eslint-disable-next-line no-unused-vars
   .on('unhandledRejection', (reason, promise) => {
-    logger.info(`Unhandled Rejection at Promise. ${reason}`);
+    logger.info(
+      `Unhandled Rejection at Promise. ${reason}. Check the log ${path.join(
+        __dirname,
+        'logs/exceptions.log'
+      )}`
+    );
   })
   .on('uncaughtException', err => {
-    logger.info(`Uncaught Exception thrown: ${err.message}`);
+    logger.info(
+      `Uncaught Exception thrown: ${err.message}. Check the log ${path.join(
+        __dirname,
+        'logs/exceptions.log'
+      )}`
+    );
   });
 
 app.listen(PORT, () =>
